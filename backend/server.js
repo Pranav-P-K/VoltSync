40f8995c812dfc01664bd9d5fe5aa621d8bb0cf9
Pranav-P-K <pranavpk1519@gmail.com>
2025-06-02 17:46:31 +0530

    leave this dont consider

--- backend/server.js
+++ backend/server.js
@@ -17,8 +17,8 @@ connectDB()
 // Middleware
 app.use(
   cors({
-    //origin: process.env.FRONTEND_URL || "http://localhost:5173",
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    //origin: "http://localhost:5173",
     credentials: true,
   }),
 )
